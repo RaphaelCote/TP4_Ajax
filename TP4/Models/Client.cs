8061ccd29789c6cbe3eb657e7ea98265fe1ef5f3
RaphaelCote <raphaelcote20@gmail.com>
2022-04-04 09:28:02 -0400

    commit

---
+++ TP4/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP4.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+
+        [Required]
+        public string Nom { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Courriel { get; set; }
+
+        [Required]
+        public string NoTelephone { get; set; }
+        public Abonnement Abonnement { get; set; }
+    }
+}
